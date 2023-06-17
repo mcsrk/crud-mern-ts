85df6c2167323b6f9153a76399fde2619b8059e8
mcsrk <49255542+mcsrk@users.noreply.github.com>
2023-06-17 01:15:38 -0500

    created a server connected to mongo with custom logger

---
+++ server/src/server.ts
@@ -0,0 +1,73 @@
+import express from 'express';
+import http from 'http';
+import mongoose from 'mongoose';
+import { config } from './config/config';
+import Logging from './library/Logging';
+import authorRoutes from './routes/Author';
+import bookRoutes from './routes/Book';
+
+const router = express();
+
+/** Connect to Mongo */
+mongoose
+    .connect(config.mongo.url, { retryWrites: true, w: 'majority' })
+    .then(() => {
+        Logging.info('Mongo connected successfully.');
+        /** If Mongoose is connected, the server will be up and running */
+        StartServer();
+    })
+    .catch((error) => Logging.error(error));
+
+/** Only Start Server if Mongoose Connects */
+const StartServer = () => {
+    /** Log the request */
+    router.use((req, res, next) => {
+        /** Log the req */
+        Logging.info(`Incomming - METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
+
+        res.on('finish', () => {
+            /** Log the res on request finished event */
+            Logging.info(`Result - METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}] - STATUS: [${res.statusCode}]`);
+        });
+
+        next();
+    });
+
+    router.use(express.urlencoded({ extended: true }));
+    router.use(express.json());
+
+    /** Rules of our API */
+    router.use((req, res, next) => {
+        /** Requests from anywhere */
+        res.header('Access-Control-Allow-Origin', '*');
+        /** Headers allowed to use */
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+        /** All of the options that can be used in this API */
+        if (req.method == 'OPTIONS') {
+            res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+            return res.status(200).json({});
+        }
+
+        next();
+    });
+
+    /** Routes */
+    router.use('/authors', authorRoutes);
+    router.use('/books', bookRoutes);
+
+    /** Healthcheck endpoint */
+    router.get('/ping', (req, res, next) => res.status(200).json({ hello: 'world' }));
+
+    /** Error handling on wrong Route */
+    router.use((req, res, next) => {
+        const error = new Error('API Route Not found');
+
+        Logging.error(error);
+
+        res.status(404).json({
+            message: error.message
+        });
+    });
+
+    http.createServer(router).listen(config.server.port, () => Logging.info(`Server is running on port ${config.server.port}`));
+};

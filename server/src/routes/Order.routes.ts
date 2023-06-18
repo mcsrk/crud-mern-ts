b5d6d8ca288714ab3592af685d728e0b7193ec23
mcsrk <49255542+mcsrk@users.noreply.github.com>
2023-06-17 20:10:03 -0500

    added comments on Order routes

--- server/src/routes/Order.routes.ts
+++ server/src/routes/Order.routes.ts
@@ -10,11 +10,13 @@ const router = express.Router();
 router.get('/', controller.readAll);
 router.get('/:orderId', controller.readOrder);
 
+/** Modify orders directly */
 router.post('/', ValidateJoi(Schemas.order.create), controller.createOrder);
 router.post('/:orderId/complete', controller.completeOrder);
 router.patch('/:orderId', CheckOrderStatus, ValidateJoi(Schemas.order.update), controller.updateOrder);
 router.delete('/:orderId', CheckOrderStatus, controller.deleteOrder);
 
+/** Modify products */
 router.post('/:orderId/products', CheckOrderStatus, ValidateJoi(Schemas.product.create), controller.addProduct);
 router.delete('/:orderId/products/:productId', CheckOrderStatus, controller.deleteProduct);
 

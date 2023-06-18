f42169217aae85beba53d1c81acc4995e035c18f
mcsrk <49255542+mcsrk@users.noreply.github.com>
2023-06-17 21:45:02 -0500

    added controller to register and login user. Encryption pending.

--- server/src/controllers/User.ts
+++ server/src/controllers/User.ts
@@ -1,19 +1,36 @@
 import { Request, Response } from 'express';
 import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+
+// Models
 import User from '../models/User';
+import { config } from '../config/config';
 
 const createUser = (req: Request, res: Response) => {
     const { username, password } = req.body;
 
-    const user = new User({
-        _id: new mongoose.Types.ObjectId(),
-        username,
-        password
-    });
+    /** Check if the username already exists*/
+    User.findOne({ username })
+        .then((existingUser) => {
+            if (existingUser) {
+                return res.status(409).json({ message: 'Username alredy taken' });
+            }
 
-    return user
-        .save()
-        .then((user) => res.status(201).json({ user }))
+            const hash = bcrypt.hashSync(password, config.auth.salt_rounds);
+
+            const user = new User({
+                _id: new mongoose.Types.ObjectId(),
+                username,
+                //password
+                password: hash
+            });
+
+            return user
+                .save()
+                .then(() => res.status(201).json({ message: 'User created successfully' }))
+                .catch((error) => res.status(500).json({ error }));
+        })
         .catch((error) => res.status(500).json({ error }));
 };
 
@@ -54,16 +71,52 @@ const deleteUser = (req: Request, res: Response) => {
     const userId = req.params.userId;
 
     return User.findByIdAndDelete(userId)
-        .then((user) => (user ? res.status(201).json({ user, message: 'Deleted' }) : res.status(404).json({ message: 'User not found' })))
+        .then((user) => (user ? res.status(204).json() : res.status(404).json({ message: 'User not found' })))
         .catch((error) => res.status(500).json({ error }));
 };
 
-const login = (req: Request, res: Response) => {
-    const userId = req.params.userId;
+const login = async (req: Request, res: Response) => {
+    try {
+        /** Get, decode and split the credentials sent in Headers*/
+        if (!req.headers.authorization) {
+            return res.status(401).send({ message: 'Credentials not found' });
+        }
 
-    return User.findByIdAndDelete(userId)
-        .then((user) => (user ? res.status(201).json({ user, message: 'Deleted' }) : res.status(404).json({ message: 'User not found' })))
-        .catch((error) => res.status(500).json({ error }));
+        const credentialsToken = req.headers.authorization.split(' ')[1];
+        const [username, password] = Buffer.from(credentialsToken, 'base64').toString().split(':');
+
+        // Check if the user exists
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(404).send({ message: 'User not found' });
+        }
+
+        // Check if the password is valid
+        // TODO: Implement password encryption and comparison
+        // const isValidPassword = await bcrypt.compare(password, user.password);
+        // if (!isValidPassword) {
+        //   return res.status(401).send({ message: 'Invalid password' });
+        // }
+
+        /** Generate a JWT token for the authenticated user */
+        const token = jwt.sign({ id: user._id, username: user.username }, config.jwt.secret, {
+            expiresIn: '30d'
+        });
+
+        if (!token) {
+            return res.status(403).send({ message: 'Invalid authentication' });
+        } else {
+            res.cookie('token', token, {
+                httpOnly: true /*,
+                secure: true,
+                signed: true,
+                maxAge: 100000*/
+            });
+            return res.status(200).send(token);
+        }
+    } catch (error) {
+        return res.status(500).send(error);
+    }
 };
 
 export default { createUser, readUser, readAll, updateUser, deleteUser, login };
